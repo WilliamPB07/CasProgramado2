b36735d57f002c18d57f3d19bc32d445c5f11716
WilliamPB07 <101308021+WilliamPB07@users.noreply.github.com>
2022-12-01 18:04:02 -0600

    faltaba FichaAzul

---
+++ CasoProgramado2/src/main/java/com/mycompany/casoprogramado2/FichaAzul.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.casoprogramado2;
+
+/**
+ *
+ * @author wpena
+ */
+public class FichaAzul  extends Ficha
+{
+    public FichaAzul()
+    {
+        super("Jugador2", 2, Colorficha.AZUL);
+    }
+    
+}
